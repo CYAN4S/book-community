063355ba5fedcabc78d46f49efb193b29ab1488c
lattesmoody <lattesmoody@gmail.com>
2022-03-06 19:09:51 +0900

    Modify home

--- pages/home.js
+++ pages/home.js
@@ -8,7 +8,7 @@ export default function Book_home() {
             </Head>
             <body>
                 <h1>Welcome!</h1>
-                <h2>오늘은 3월 6일 입니다.</h2>
+                <h2>오늘은 3월 6일 입니다!</h2>
             </body>
     </html>
     )
