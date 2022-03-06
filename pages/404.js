cf08d5ef333371737ae26b202ed2699d3e4238a1
Lee-Sang-Beom <tkdqja975@naver.com>
2022-03-06 18:45:02 +0900

    install react-ui-css and add 404 page

---
+++ pages/404.js
@@ -0,0 +1,13 @@
+import { Icon } from "semantic-ui-react";
+
+const Error404 = () => {
+    return (
+        <div style={{padding : "200px 0", textAlign : "center", fontSize : "35px"}}>
+            <Icon name="warning circle" color="red"/> 404 error
+        </div>
+    )
+}
+
+
+
+export default Error404;
