2c5eb1e7716341182757c7605c5bdf6c9cac5ff5
Lee-Sang-Beom <tkdqja975@naver.com>
2022-03-18 19:39:03 +0900

    install firebase

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,27 @@
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { authService } from "../firebaseConfig";
 import Sign from "./sign";
 
 export default function Home() {
   const [isSigned, setIsSigned] = useState(false);
   const [init, setInit] = useState(true);
+  const [userObj, setUserObj] = useState(null);
 
-  
+  useEffect(()=>{
+    authService.onAuthStateChanged((user) => {
+      console.log(user);
+      if(user){
+        // 로그인 되어 있다면
+        setIsSigned(true);
+      }else{
+        // 로그인 되어있지 않다면
+        setIsSigned(false);
+      }
+      setInit(true);
+    })
+  }, [])
+
   return (
    <>
     {init ? <Sign/> : "Initializing.."}
