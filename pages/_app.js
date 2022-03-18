96e1d99864d999693251ada0c714a6da46d8fdc3
Lee-Sang-Beom <tkdqja975@naver.com>
2022-03-18 19:20:41 +0900

    install firebase

--- pages/_app.js
+++ pages/_app.js
@@ -3,8 +3,6 @@ import 'semantic-ui-css/semantic.min.css'
 
 
 // TODO: Replace the following with your app's Firebase project configuration
-
-
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,8 @@ import Sign from "./sign";
 export default function Home() {
   const [isSigned, setIsSigned] = useState(false);
   const [init, setInit] = useState(true);
+
+  
   return (
    <>
     {init ? <Sign/> : "Initializing.."}
