7a200371ecec6d1dc01f16bfb092f335552615ca
Lee-Sang-Beom <tkdqja975@naver.com>
2022-04-01 18:32:31 +0900

    add navbar, about page

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
 import '../styles/globals.css'
 import 'semantic-ui-css/semantic.min.css'
-
+import Navigation from "../Components/Navigation"
 
 // TODO: Replace the following with your app's Firebase project configuration
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div style={{margin : '10px'}}>
+      <Navigation />
+      <Component {...pageProps} />
+    </div>
+    
+  )
 }
 export default MyApp
