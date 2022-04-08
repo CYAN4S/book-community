01e41a32fa5876c00832c5bae1d05503bb503e93
Lee-Sang-Beom <tkdqja975@naver.com>
2022-04-08 18:02:23 +0900

    add edit and del

--- Components/Chats.js
+++ Components/Chats.js
@@ -1,18 +1,87 @@
 
-import { collection, getDocs, onSnapshot, query } from "firebase/firestore";
+import { collection, deleteDoc, doc, getDocs, onSnapshot, query, updateDoc } from "firebase/firestore";
 import React, { useState, useEffect } from "react";
-import { authService, dbService } from "../firebaseConfig";
+import { Button } from "semantic-ui-react";
+import { authService, dbService, storageService } from "../firebaseConfig";
 
-export default function Chats({ chat }) {
+export default function Chats({ chat, isOwner }) {
 
-    const newChat = chat.text;
-    const username = chat.nickName ? chat.nickName : "guest";
-    
+    const [newChat, setNewChat] = useState(chat.text);
+    const [username,setUserName] = useState(chat.nickName ? chat.nickName : "guest");
+    const [editing, setEditing] = useState(false);
+
+    const onDeleteClick = async() =>{
+        const ok = window.confirm("채팅을 삭제하시겠습니까?");
+        if(ok){
+            await deleteDoc(doc(dbService, "chat", `${chat.id}`));            
+        }
+    }
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        updateDoc(doc(dbService, "chat", `${chat.id}`),{
+            text : newChat
+        }).then(() => {
+            alert("수정되었습니다!");
+          })
+          .catch((error) => {
+            alert(error);
+          });
+        setEditing(false);
+
+    }
+
+    const onChange = (event) =>{
+        const {
+            target : {value}
+        } = event;
+        setNewChat(value);
+    }
+
+
+    const toggleEditing = () => setEditing((prev) => !prev);
 
         return (
+            editing ? (
+                <div>
+                    <form onSubmit={onSubmit} > 
+                        <input 
+                            value = {newChat} 
+                            type = "text"
+                            placeholder="수정하기"
+                            onChange={onChange}
+                            autoFocus
+                            required/>
+
+                        <Button type = "submit" value="update"> 수정 완료 </Button>
+                    </form> 
+                    <Button onClick = {toggleEditing}>
+                        cancel
+                    </Button>
+                </div>)
+                :
             <div>
-                {username} : {newChat}
+                <div>
+                {username} : <strong> {chat.text}</strong>
+                    {isOwner ? 
+                    <span className="btn_span">
+                        <Button onClick={onDeleteClick}>삭제</Button>
+                        <Button onClick={toggleEditing}>편집</Button>
+                    </span>
+                    : <></>
+                    }
+                </div>
+                <style jsx>{`
+                    .btn_span{
+                        margin-left:10px;
+                    }
+
+                    strong{
+                        font-size:15px;
+                    }
+                `}</style>
             </div>
+            
         );
 
 }
--- pages/home.js
+++ pages/home.js
@@ -1,54 +1,77 @@
 import Head from "next/head";
 import { authService, dbService } from "../firebaseConfig";
 import { useRouter } from "next/router";
-import { Button, Divider} from "semantic-ui-react";
+import { Button, Divider, Header } from "semantic-ui-react";
 import { useState, useEffect } from "react";
 import { collection, getDocs, onSnapshot, orderBy, query } from "firebase/firestore";
 import ChatFactory from "../Components/ChatFactory";
 import Chats from "../Components/Chats";
 import { async } from "@firebase/util";
+import { onAuthStateChanged } from "firebase/auth";
 
 export default function Book_home() {
   const router = useRouter();
-
-  const [chats, SetChats] = useState([]);
+  const [chats, setChats] = useState([]);
+  const [userId, setUserId] = useState("");
+  onAuthStateChanged(authService, (user) => {
+    if (user) {
+      setUserId(user.uid);
+    }
+  });
 
   function onLogOutClick() {
     authService.signOut();
     router.push("/");
   }
 
-  const q = query(collection(dbService, "chat"),orderBy("createdAt","desc"));
-  useEffect(()=>{
+  const q = query(collection(dbService, "chat"), orderBy("createdAt", "desc"));
+  useEffect(() => {
 
-    onSnapshot(q,snapshot =>{
-        const chatArray = snapshot.docs.map((doc) => ({
-            id : doc.id,
-            ...doc.data(),
-        }));
+    onSnapshot(q, snapshot => {
+      const chatArray = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
 
-        SetChats(chatArray);
-        // dbservice를 이용해 sweets 컬렉션의 변화를 실시간으로 확인. 변화발생 때 마다 console.log
+      setChats(chatArray);
+      // dbservice를 이용해 sweets 컬렉션의 변화를 실시간으로 확인. 변화발생 때 마다 console.log
     })
-},[])
+  }, [])
 
-  
+
   return (
-    <div>
+    <>
+      <div>
       <Head>
         <title>Home pages</title>
       </Head>
       <div>
-        <ChatFactory/>
-        <Divider/>
+        <ChatFactory />
+
+        <Divider />
+        <Header as="h2">올라온 채팅</Header>
         <div>
-        {chats.map((chat)=>(
-          <Chats chat={chat} key = {chat.id}/>
-        ))}
-                
+          {chats.map((chat) => (
+            <div className="chat_space">
+              <Chats chat={chat} key={chat.id} isOwner={chat.createrId === userId} />
+            </div>
+          ))}
         </div>
+
+        <Divider />
+        <Header as="h2">로그아웃 하기</Header>
         <Button onClick={onLogOutClick}> Logout </Button>
       </div>
     </div>
+    <style jsx>{`
+        .chat_space{
+          margin-left : 10px;
+          margin-bottom : 5px;
+          width : 300px;
+          padding : 10px 10px 10px 0px;
+        }
+      `}</style>
+    </>
+    
   );
 }
