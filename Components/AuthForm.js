72ace4dc9288b69d1de3ca8cca5b64948aad92b2
Lee-Sang-Beom <tkdqja975@naver.com>
2022-04-09 23:20:49 +0900

    notify sign page 2

--- Components/AuthForm.js
+++ Components/AuthForm.js
@@ -30,46 +30,79 @@ export default function AuthForm() {
   const toggleAccount = () => setNewAccount(!newAccount);
 
   return (
+  
     <>
-      <Header as="h3" style={{color:"white", background : "black", borderRadius:10}}>{newAccount ? "CreateAccount" : "Login"}</Header>
-      <Form onSubmit={onSubmit} className="container" style={{marginBottom:10}}>
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          name="email"
-          onChange={(e) => {
-            setEmail(e.target.value);
-          }}
-          className="authInput"
-          required
-        />
+      <div>
+        <Header
+          as="h3"
+          style={{ color: "white", background: "black", borderRadius: 10 }}
+        >
+          {newAccount ? "CreateAccount" : "Login"}
+        </Header>
+        <Form
+          onSubmit={onSubmit}
+          className="container"
+          style={{ marginBottom: 10 }}
+        >
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            name="email"
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            className="authInput"
+            required
+          />
 
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          name="password"
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-          className="authInput"
-          required
-        />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            name="password"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            className="authInput"
+            required
+          />
 
-        <span style={{display:"flex", alignItems:"center"}}>
-          <Button
-            type="submit"
-            value={newAccount ? "Create Account" : "Log in"}
-            style={{ marginTop: 10, marginBottom: 10 }}
-            className="authInput authSubmit">
-            시작하기
-          </Button>
-          <p onClick={toggleAccount} style={{marginLeft:5, borderBottom:"1px solid black", cursor : "pointer"}}> {newAccount ? "로그인으로" : "회원가입으로"} </p>
-        </span>
-      </Form>
+          <span style={{ display: "flex", alignItems: "center" }}>
+            <Button
+              type="submit"
+              value={newAccount ? "Create Account" : "Log in"}
+              style={{ marginTop: 10, marginBottom: 10 }}
+              className="authInput authSubmit"
+            >
+              시작하기
+            </Button>
+            <p onClick={toggleAccount} className="changeBtn">
+              {newAccount ? "로그인으로" : "회원가입으로"}{" "}
+            </p>
+          </span>
+        </Form>
 
-      {error && <><strong style={{color: "red"}}>오류발생!</strong> <p style={{marginLeft:10}}>{error}</p></>}
+        {error && (
+          <>
+            <strong style={{ color: "red" }}>오류발생!</strong>{" "}
+            <p style={{ marginLeft: 10 }}>{error}</p>
+          </>
+        )}
+      </div>
+      <style jsx>{`
+              .changeBtn {
+                margin-left : 5px;
+                border-Bottom : 1px solid black;
+                cursor : pointer;
+                font-size : 15px;
+              }
+
+              .changeBtn:hover {
+                font-weight : bold;
+              }
+            `}</style>
+      
     </>
   );
 }
