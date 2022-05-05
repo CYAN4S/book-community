3addda34165e3077204978e14e17b8f872f286bf
lattesmoody <lattesmoody@gmail.com>
2022-05-05 14:36:43 +0900

    0505 1436

--- pages/profile/[id].jsx
+++ pages/profile/[id].jsx
@@ -38,6 +38,8 @@ export default function Profile() {
 
   const [subscribers, setSubscribers] = useState([]);
 
+  const [myBooks, setmyBooks] = useState([]);
+
   const isMe = () => currentUid == queryId;
 
   onAuthStateChanged(auth, (user) => {
