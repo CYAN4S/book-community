aea502ee6b5eb663e9c775422253d9c7ce5834c3
Lee-Sang-Beom <tkdqja975@naver.com>
2022-05-06 15:41:21 +0900

    help page add container

--- pages/view/help.js
+++ pages/view/help.js
@@ -1,29 +1,50 @@
 import {
-    Button,
-    Card,
-    Divider,
-    Form,
-    Header,
-    Input,
-    List,
-    Placeholder,
-    Segment,
-    Sidebar,
-    TextArea,
-    Image,
-    Menu,
-    Icon,
-    Label,
-    Grid,
-    Checkbox,
-    Item,
-  } from "semantic-ui-react";
-  import React from "react";
-  import Link from "next/link";
+  Button,
+  Card,
+  Container,
+  Divider,
+  Form,
+  Header,
+  Input,
+  List,
+  Placeholder,
+  Segment,
+  Sidebar,
+  TextArea,
+  Image,
+  Menu,
+  Icon,
+  Label,
+  Grid,
+  Checkbox,
+  Item,
+} from "semantic-ui-react";
+import React from "react";
+
 const Help = () => {
   return (
     <div>
-     1
+      <Container fluid>
+        <Header as="h2">Dogs Roles with Humans</Header>
+        <p>
+          Domestic dogs inherited complex behaviors, such as bite inhibition,
+          from their wolf ancestors, which would have been pack hunters with
+          complex body language. These sophisticated forms of social cognition
+          and communication may account for their trainability, playfulness, and
+          ability to fit into human households and social situations, and these
+          attributes have given dogs a relationship with humans that has enabled
+          them to become one of the most successful species on the planet today.
+        </p>
+        <p>
+          The dogs' value to early human hunter-gatherers led to them quickly
+          becoming ubiquitous across world cultures. Dogs perform many roles for
+          people, such as hunting, herding, pulling loads, protection, assisting
+          police and military, companionship, and, more recently, aiding
+          handicapped individuals. This impact on human society has given them
+          the nickname "man's best friend" in the Western world. In some
+          cultures, however, dogs are also a source of meat.
+        </p>
+      </Container>
     </div>
   );
 };
