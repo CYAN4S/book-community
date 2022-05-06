3971320b69199d1a7cef54499d9e08bb10d37f6f
Lee-Sang-Beom <tkdqja975@naver.com>
2022-05-06 23:58:58 +0900

    notify help page- add grid

--- pages/view/help.js
+++ pages/view/help.js
@@ -18,34 +18,89 @@ import {
   Grid,
   Checkbox,
   Item,
+  Popup,
+  Rating,
 } from "semantic-ui-react";
 import React from "react";
 
 const Help = () => {
   return (
-    <div>
-      <Container fluid>
-        <Header as="h2">Dogs Roles with Humans</Header>
-        <p>
-          Domestic dogs inherited complex behaviors, such as bite inhibition,
-          from their wolf ancestors, which would have been pack hunters with
-          complex body language. These sophisticated forms of social cognition
-          and communication may account for their trainability, playfulness, and
-          ability to fit into human households and social situations, and these
-          attributes have given dogs a relationship with humans that has enabled
-          them to become one of the most successful species on the planet today.
-        </p>
-        <p>
-          The dogs' value to early human hunter-gatherers led to them quickly
-          becoming ubiquitous across world cultures. Dogs perform many roles for
-          people, such as hunting, herding, pulling loads, protection, assisting
-          police and military, companionship, and, more recently, aiding
-          handicapped individuals. This impact on human society has given them
-          the nickname "man's best friend" in the Western world. In some
-          cultures, however, dogs are also a source of meat.
-        </p>
-      </Container>
-    </div>
+    <>
+      <div>
+        <Container fluid>
+          <Header as="h2">도움말 페이지</Header>
+          <Grid columns={4}>
+            <Grid.Row>
+              <Grid.Column>
+                <Popup
+                  trigger={
+                    <Card>
+                      <Image src="/bookstamp.png" />
+                      <Card.Content>
+                        <Card.Header>COMMUNITY</Card.Header>
+                        <Card.Description>사용자와 대화하기</Card.Description>
+                      </Card.Content>
+                    </Card>
+                  }
+                >
+                  <Popup.Header>확인하기</Popup.Header>
+                </Popup>
+              </Grid.Column>
+              <Grid.Column>
+                <Popup
+                  trigger={
+                    <Card>
+                      <Image src="/bookstamp.png" />
+                      <Card.Content>
+                        <Card.Header>COMMUNITY</Card.Header>
+                        <Card.Description>사용자와 대화하기</Card.Description>
+                      </Card.Content>
+                    </Card>
+                  }
+                >
+                  <Popup.Header>확인하기</Popup.Header>
+                </Popup>
+              </Grid.Column>
+              <Grid.Column>
+                <Popup
+                  trigger={
+                    <Card>
+                      <Image src="/bookstamp.png" />
+                      <Card.Content>
+                        <Card.Header>COMMUNITY</Card.Header>
+                        <Card.Description>사용자와 대화하기</Card.Description>
+                      </Card.Content>
+                    </Card>
+                  }
+                >
+                  <Popup.Header>확인하기</Popup.Header>
+                </Popup>
+              </Grid.Column>
+              <Grid.Column>
+                <Popup
+                  trigger={
+                    <Card>
+                      <Image src="/bookstamp.png" />
+                      <Card.Content>
+                        <Card.Header>COMMUNITY</Card.Header>
+                        <Card.Description>사용자와 대화하기</Card.Description>
+                      </Card.Content>
+                    </Card>
+                  }
+                >
+                  <Popup.Header>확인하기</Popup.Header>
+                </Popup>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Container>
+      </div>
+      <style jsx>{`
+        div {
+          display: flex;
+        }
+      `}</style>
+    </>
   );
 };
 
