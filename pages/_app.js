50627909c649860868a16a2f33f57a798ddf5f9f
Lee-Sang-Beom <tkdqja975@naver.com>
2022-05-15 17:37:08 +0900

    add icon in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import "../styles/globals.css";
 import "semantic-ui-css/semantic.min.css";
 import Navigation from "../Components/Navigation";
 import Script from "next/script";
-import { Header } from "semantic-ui-react";
+import { Header, Icon } from "semantic-ui-react";
 import "../public/static/fonts/style.css";
 // React
 import React from "react";
@@ -25,9 +25,10 @@ function MyApp({ Component, pageProps }) {
       <RecoilComponent />
       <div >
         <div className="main">
-          <Header as="h1" inverted color="blue" style={{marginTop : 30, marginLeft : 10}}>
+          <Header as="h1" inverted color="violet" style={{marginTop : 30, marginLeft : -30}}>
+          <Icon name ="volume up" size='small'/>
           <Header.Content style={{marginLeft : -50, fontSize: 20, fontFamily : "Gugi-Regular"}}>새로운 소통을 꿈꾸는</Header.Content>
-            <p style = {{fontFamily: "GamjaFlower-Regular", marginTop : -15, marginBottom:30, fontSize: 40}}>BOOKSTAMP</p>
+            <p style = {{fontFamily: "GamjaFlower-Regular", marginTop : -15, marginLeft : 70, marginBottom:30, fontSize: 40}}>BOOKSTAMP</p>
             
           </Header>
         </div>
