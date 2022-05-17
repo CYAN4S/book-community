f05227cfbe4ca9db12f1e14a6572e238d5dd95ff
lattesmoody <lattesmoody@gmail.com>
2022-05-17 23:26:03 +0900

    init

--- pages/home.js
+++ pages/home.js
@@ -9,6 +9,9 @@ import Chats from "../Components/Chats";
 import { onAuthStateChanged } from "firebase/auth";
 
 export default function Book_home() {
+  // userPhoto Real-time synchronization
+  const [userPhotos, setUserPhotos] = useState([]);
+
   const [chats, setChats] = useState([]);
   const [userId, setUserId] = useState("");
 
@@ -31,17 +34,29 @@ export default function Book_home() {
     });
   }, []);
 
+  // 0517_2145 home snapshot(사용자프로필, 닉네임 부분 useEffect 활성화) code START
+  const userPhotoQuery = query(collection(dbService, "profile"));
+  useEffect(() => {
+    onSnapshot(userPhotoQuery, (snapshot) => {
+      const userPhotoArray = [];
+      snapshot.forEach((doc) => {
+        userPhotoArray.push(doc.data().userPhoto);
+      });
+      setUserPhotos(userPhotoArray);
+      // dbservice를 이용해 sweets 컬렉션의 변화를 실시간으로 확인.
+    });
+  }, []);
+  // 0517_2145 home snapshot(사용자프로필, 닉네임 부분 useEffect 활성화) code END
+  console.log(userPhotos);
   return (
     <>
-      <div style={{marginLeft : 5}}>
+      <div style={{ marginLeft: 5 }}>
         <Head>
           <title>Home pages</title>
         </Head>
-        <div style={{height:30}}>
-         
-        </div>
+        <div style={{ height: 30 }}></div>
         <div>
-          <Header as="h2" style ={{marginTop : -40}}>
+          <Header as="h2" style={{ marginTop: -40 }}>
             <Icon name="chat" />
             <Header.Content>
               의견남기기
@@ -56,14 +71,16 @@ export default function Book_home() {
             <Icon name="wechat" />
             <Header.Content>
               올라온 채팅
-              <Header.Subheader>다른 사용자의 채팅을 볼 수 있어요!</Header.Subheader>
+              <Header.Subheader>
+                다른 사용자의 채팅을 볼 수 있어요!
+              </Header.Subheader>
             </Header.Content>
           </Header>
-          <div style={{marginTop:30}}>
+          <div style={{ marginTop: 30 }}>
             {chats.length ? (
               chats.map((chat) => (
-                <div key={chat.id} style={{marginBottom:30}}>
-                  <Chats chat={chat} isOwner={chat.createrId === userId} />
+                <div key={chat.id} style={{ marginBottom: 30 }}>
+                  <Chats chat={chat} isOwner={chat.createrId === userId} userphoto = {chat} />
                 </div>
               ))
             ) : (
