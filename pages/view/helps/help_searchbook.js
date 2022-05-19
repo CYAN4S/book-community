d8be268f07d1768140120db46e669093d447c356
Lee-Sang-Beom <tkdqja975@naver.com>
2022-05-19 14:52:20 +0900

    complete help_searchbook page

--- pages/view/helps/help_searchbook.js
+++ pages/view/helps/help_searchbook.js
@@ -4,7 +4,6 @@ import {
   Form,
   Header,
   Input,
-
   Segment,
   TextArea,
   Icon,
@@ -60,39 +59,69 @@ const Help_SearchBook = () => {
             </Header.Subheader>
           </Header.Content>
         </Header>
+      </Container>
 
-        <Grid celled  verticalAlign='middle' >
+      <Container textAlign="center" fluid style={{ marginTop: 10 }}>
+        <Header as="h4">
+          <Header.Content>
+            <Header.Subheader>
+              <p
+                style={{ fontSize: 11, color: "red", marginTop : 30 }}
+              >{`현재 개발중인 내용으로, 이미지와 설명은 변경될 수 있습니다.`}</p>
+            </Header.Subheader>
+          </Header.Content>
+        </Header>
+      </Container>
+
+      <Container textAlign="left" fluid>
+        <Grid celled verticalAlign="middle" centered>
           <Grid.Row>
             <Grid.Column width={9}>
-              <Image src={help1} width={800} height={100}/>
+              <Image src={help1} width={900} height={120} />
             </Grid.Column>
-            <Grid.Column centered width={6}>
-              <p>hello</p>
+            <Grid.Column centered width={7}>
+              <p className="help_searchbook_text">
+                {`1. Explore 탭을 통해, 검색 창으로 이동할 수 있습니다.`}
+              </p>
+             
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column width={9}>
+              <Image src={help2} width={900} height={170} />
+            </Grid.Column>
+            <Grid.Column centered width={7}>
+              <p className="help_searchbook_text">
+              {`2. Explore 페이지의 검색박스와 검색 버튼의 클릭을 통해, 특정 도서의
+                검색 결과를 확인할 수 있습니다.`}
+              </p>
             </Grid.Column>
           </Grid.Row>
 
           <Grid.Row>
             <Grid.Column width={9}>
-            <Image src={help2} width={800} height={170}/>
+              <Image src={help3} width={900} height={330} />
             </Grid.Column>
-            <Grid.Column centered width={6}>
-            <p>hello</p>
-            </Grid.Column>
-          </Grid.Row>
-
-          <Grid.Row>
-            <Grid.Column width={9}>
-            <Image src={help3} width={800} height={270}/>
-            </Grid.Column>
-            <Grid.Column centered width={6}>
-            <p>hello</p>
+            <Grid.Column centered width={7}>
+              <p className="help_searchbook_text">
+              {`3. 도서 검색 결과를 필터별로 확인할 수 있으며, 출력된 도서를
+                클릭하여, 세부정보를 확인할 수 있습니다.`}
+              </p>
             </Grid.Column>
           </Grid.Row>
         </Grid>
-
       </Container>
 
-      <style jsx>{``}</style>
+      <style jsx>{`
+        div {
+          margin-top: 30px;
+          text-align: right;
+        }
+        .help_searchbook_text {
+          font-size: 15px;
+          font-family: GothicA1-Medium;
+        }
+      `}</style>
     </>
   );
 };
