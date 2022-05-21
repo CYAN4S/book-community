18448b242de6d8c2c0f006c1904fdf2478d0e08c
Lee-Sang-Beom <tkdqja975@naver.com>
2022-05-21 10:53:05 +0900

    add css : help_subscriber

--- pages/view/helps/help_subscriber.js
+++ pages/view/helps/help_subscriber.js
@@ -59,6 +59,57 @@ import {
           </Header>
         </Container>
   
+        <Container textAlign="center" fluid style={{ marginTop: 10 }}>
+        <Header as="h4">
+          <Header.Content>
+            <Header.Subheader>
+              <p
+                style={{ fontSize: 11, color: "red", marginTop : 30 }}
+              >{`현재 개발중인 내용으로, 이미지와 설명은 변경될 수 있습니다.`}</p>
+            </Header.Subheader>
+          </Header.Content>
+        </Header>
+      </Container>
+
+      <Container textAlign="left" fluid  style={{width : "95%"}}>
+        <Grid celled verticalAlign="middle" centered>
+          <Grid.Row>
+            <Grid.Column width={9}>
+              {/* <Image src={help1} width={900} height={120} /> */}
+            </Grid.Column>
+            <Grid.Column centered width={7}>
+              <p className="help_searchbook_text">
+                {`1. Explore 탭을 통해, 검색 창으로 이동할 수 있습니다.`}
+              </p>
+             
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column width={9}>
+              {/* <Image src={help2} width={900} height={170} /> */}
+            </Grid.Column>
+            <Grid.Column centered width={7}>
+              <p className="help_searchbook_text">
+              {`2. Explore 페이지의 검색박스와 검색 버튼의 클릭을 통해, 특정 도서의
+                검색 결과를 확인할 수 있습니다.`}
+              </p>
+            </Grid.Column>
+          </Grid.Row>
+
+          <Grid.Row>
+            <Grid.Column width={9}>
+              {/* <Image src={help3} width={900} height={330} /> */}
+            </Grid.Column>
+            <Grid.Column centered width={7}>
+              <p className="help_searchbook_text">
+              {`3. 도서 검색 결과를 필터별로 확인할 수 있으며, 출력된 도서를
+                클릭하여, 세부정보를 확인할 수 있습니다.`}
+              </p>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Container>
+
         <style jsx>{`
           div {
             margin-top: 30px;
