664e6de37dad5009fea92409afff2a55b29141b4
lattesmoody <lattesmoody@gmail.com>
2022-05-22 20:07:09 +0900

    reload() => pathname

--- Components/PostEditor.jsx
+++ Components/PostEditor.jsx
@@ -187,7 +187,8 @@ export default function PostEditor({
       const url = window.location.href;
       if (!url.includes("home")) {
         if (!url.includes("post")){
-        router.push(window.location.reload());
+        //router.push(window.location.reload());
+        router.reload(window.location.pathname);
         }
         else{
           router.back();
