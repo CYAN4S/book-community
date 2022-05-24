88c16478c6847c3c3b8f95eb7b7f021c770c5681
Lee-Sang-Beom <tkdqja975@naver.com>
2022-05-24 20:47:02 +0900

    Simplify animations

--- public/HOC/withTransition.js
+++ public/HOC/withTransition.js
@@ -5,29 +5,22 @@ const withTransition = (OriginalComponent) => {
   return () => (
     <>
       <OriginalComponent />
-      <motion.div
-        className="slide-in"
-        initial={{ scaleX: 0 }}
-        animate={{ scaleX: 0 }}
-        exit={{ scaleX: 1 }}
-        transition={{ duration: 1, ease: "easeInOut" }}
-      />
       {useRouter().pathname =="/main" ? <>
       <motion.div
         className="slide-out"
-        initial={{ scaleX: 1 }}
-        animate={{ scaleY: 0 }}
-        exit={{ scaleX: 0 }}
+        initial={{ scaleX: 1 }} // 1s 후
+        animate={{ scaleY: 0 }} // direction
+        exit={{ scaleY: 0 }}
         transition={{ duration: 0.8, ease: "easeInOut" }}
       />
       </> : <>
-      <motion.div
+      {/* <motion.div
         className="slide-out"
         initial={{ scaleX: 1 }}
         animate={{ scaleX: 0 }}
         exit={{ scaleX: 0 }}
-        transition={{ duration: 0.5, ease: "easeInOut" }}
-      />
+        transition={{ duration: 0.8, ease: "easeInOut" }}
+      /> */}
       </>}
       
     </>
