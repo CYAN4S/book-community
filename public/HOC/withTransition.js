4b23d2cedfb4c4aee243062427179741404f538e
Lee-Sang-Beom <tkdqja975@naver.com>
2022-05-24 20:34:31 +0900

    change animate

--- public/HOC/withTransition.js
+++ public/HOC/withTransition.js
@@ -1,6 +1,7 @@
 import { motion } from "framer-motion";
-
+import { useRouter } from "next/router";
 const withTransition = (OriginalComponent) => {
+
   return () => (
     <>
       <OriginalComponent />
@@ -11,13 +12,24 @@ const withTransition = (OriginalComponent) => {
         exit={{ scaleX: 1 }}
         transition={{ duration: 1, ease: "easeInOut" }}
       />
+      {useRouter().pathname =="/main" ? <>
+      <motion.div
+        className="slide-out"
+        initial={{ scaleX: 1 }}
+        animate={{ scaleY: 0 }}
+        exit={{ scaleX: 0 }}
+        transition={{ duration: 0.8, ease: "easeInOut" }}
+      />
+      </> : <>
       <motion.div
         className="slide-out"
         initial={{ scaleX: 1 }}
         animate={{ scaleX: 0 }}
         exit={{ scaleX: 0 }}
-        transition={{ duration: 1, ease: "easeInOut" }}
+        transition={{ duration: 0.5, ease: "easeInOut" }}
       />
+      </>}
+      
     </>
   );
 };
