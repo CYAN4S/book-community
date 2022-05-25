9c7b3e74091373fefb706a256a4dba0f58a6e41b
Lee-Sang-Beom <tkdqja975@naver.com>
2022-05-25 16:20:19 +0900

    add motion.btn in main

--- pages/main.js
+++ pages/main.js
@@ -9,18 +9,41 @@ import {
 } from "semantic-ui-react";
 import { useRouter } from "next/router";
 import withTransition from "../public/HOC/withTransition";
+import { motion } from 'framer-motion';
+import { useState } from "react";
 
  function Main() {
   const router = useRouter();
-
+  const [clicked, setClicked] = useState(false);
   // When click event occurs, go to WE page
   const onClick = () => {
+    setClicked(true);
     router.push("/we");
   }
 
+  const onClick_goDiscord = () => {
+    setClicked(true);
+    router.push("/we");
+  }
+
+  const buttonVariants = {
+    // function으로 정의하는 모습
+    hover: (clicked) => ({
+      // 클릭된 버튼은 scale이 커지지 않는다.
+      scale: clicked ? 1 : 1.5,
+      color : "white",
+      backgroundColor : "black"
+    }),
+    pressed: {
+      scale: 0.7,
+    },
+    rest: {
+      scale: 1,
+    },
+  };
+
   // header component
   const HomepageHead = () => (
-
     <Container text>
       <Header
         as="h1"
@@ -44,11 +67,34 @@ import withTransition from "../public/HOC/withTransition";
           marginBottom: "11.5em",
         }}
       />
-      <Button inverted size="huge" onClick={onClick}>
-        Get Started
-        <Icon name="right arrow" />
-      </Button>
+      <div>
+        <motion.button
+          initial="rest"
+          whileHover="hover" // hover상태 일 때 hover animation발생
+          whileTap="pressed"
+          variants={buttonVariants}
+          custom={clicked} // custom을 통해 값을 전달 할 수 있다.
+          onClick={onClick}
+          style={{backgroundColor :"white", cursor : "pointer", paddingLeft : 10, paddingTop:5, marginBottom : 5, fontSize : 25, fontFamily : "FredokaOne-Regular"}}
+        >
+          Go Bookstamp
+          <Icon name="right arrow" style={{marginLeft :10}}/>
+        </motion.button>
 
+        <motion.button
+          initial="rest"
+          whileHover="hover" // hover상태 일 때 hover animation발생
+          whileTap="pressed"
+          variants={buttonVariants}
+          custom={clicked} // custom을 통해 값을 전달 할 수 있다.
+          onClick={onClick_goDiscord}
+          style={{backgroundColor :"white", cursor : "pointer", paddingLeft : 10, paddingTop:5, marginBottom : 5, fontSize : 25, fontFamily : "FredokaOne-Regular"}}
+        >
+          Go Discord
+          <Icon name="discord" style={{marginLeft :10}}/>
+        </motion.button>
+      </div>
+     
     </Container>
   );
 
